ca49c4ad2e45c9506a60c43375a49fa8663815bb
Jethro Kuan <jethrokuan95@gmail.com>
2016-04-10 18:44:21 +0800

    Add "Language Detection"

---
+++ uva/12250/lang-detection.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  char s[20];
+  int i = 1;
+  while (scanf("%s", s) && strcmp(s, "#") != 0) {
+    printf("Case %d: ", i);
+    if (strcmp(s, "HELLO") == 0) {
+      printf("ENGLISH\n");
+    } else if (strcmp(s, "HOLA") == 0) {
+      printf("SPANISH\n");
+    } else if (strcmp(s, "HALLO") == 0) { 
+      printf("GERMAN\n");
+    } else if (strcmp(s, "BONJOUR") == 0) {
+      printf("FRENCH\n");
+    } else if (strcmp(s, "CIAO") == 0) {
+      printf("ITALIAN\n");
+    } else if (strcmp(s, "ZDRAVSTVUJTE") == 0) {
+      printf("RUSSIAN\n");
+    } else {
+      printf("UNKNOWN\n");
+    }
+    i++;
+  }
+
+  return 0;
+}
