29f0f5c4b3e65f2295b171dbbee66324bedc0db6
Jethro Kuan <jethrokuan95@gmail.com>
2016-04-26 13:22:28 +0800

    Update code for catfight

--- oj/247_catfight.cpp
+++ oj/247_catfight.cpp
@@ -8,23 +8,17 @@ int main() {
   long long n, p;
   long long m = 0;
   long long sum = 0;
-  long long c = 0;
   scanf("%lld %lld", &n, &p);
   for (long long i=0,t; i<n; i++) {
     scanf("%lld", &t);
     sum += t;
-    c++;
     x.push_back(t);
-    if (sum < p) {      
-      if (c > m) m = c;      
-    } else {
-      while (sum > p && x.size() != 0) {
-        long long v = x.front();
-        sum -= v;
-        c--;
-        x.pop_front();
-      }
+    while (sum > p && x.size() != 0) {
+      sum -= x.front();
+      x.pop_front();
     }
+     
+    m = max(m, (long long int)(x.size()));
   }
 
   printf("%lld", m);
